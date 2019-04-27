008e254322bb75d20d7ccd8af816170a3a8d7f02
Tumdash <nickolay.kovalev@gmail.com>
2019-04-27 14:04:37 +0300

    draft based on XML templates

--- src/winerlm.erl
+++ src/winerlm.erl
@@ -37,7 +37,38 @@
 ) ->
     {ok, CIMData :: map()} | {error, Reason :: term()}.
 get_cim_instance(ComputerName, Options, ClassName, KeyIdentity) ->
-    {error, not_implemented}.
+	Uuid = <<"4E6FD101-710A-41EA-A50D-70C0BF863AA2">>,
+	EnumerateMsg = winerlm_xml:enumerate(ComputerName, ClassName, Uuid),
+	Destination = <<"http://", ComputerName/binary, ":5985/wsman">>,
+	Application = "application/soap+xml;charset=UTF-8",
+	UserName = binary_to_list(maps:get(user, Options)),
+	Password = binary_to_list(maps:get(password, Options)),
+	EnumResponse = ntlm_httpc:request_basic(
+		post,
+		{binary_to_list(Destination), [], Application, binary_to_list(EnumerateMsg)},
+		{"na", "na", UserName, Password}
+	),
+	case handle_response(EnumResponse) of
+		{enumeration_resp, EnumContext} ->
+			PullMsg = winerlm_xml:pull(ComputerName, ClassName, EnumContext, 1, Uuid),
+			PullResponse = ntlm_httpc:request_basic(
+				post,
+				{binary_to_list(Destination), [], Application, binary_to_list(PullMsg)},
+				{"na", "na", UserName, Password}
+			),
+			case handle_response(PullResponse) of
+				{pull_resp, ItemData} ->
+					{ok, ItemData};
+				{failure, Error} ->
+					{error, Error}
+			end;
+		{failure, Error} ->
+			{error, Error}
+	end.
+
+%TODO handle errors always a good idea
+handle_response({ok, {_, _, XmlBody}}) ->
+	winerlm_xml:parse_response(XmlBody).
 
 %% Filters specified CIM ClassNames with specified WQL from remote or local
 %% CIM-based system i.e.
